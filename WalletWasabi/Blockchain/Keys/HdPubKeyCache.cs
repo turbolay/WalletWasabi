b75679d03d41f4d4ffa1c2cf6ee14697186993d8
Turbolay <clement.ogame@gmail.com>
2024-01-12 21:53:44 -0600

    Remove all except dictionary

--- WalletWasabi/Blockchain/Keys/HdPubKeyCache.cs
+++ WalletWasabi/Blockchain/Keys/HdPubKeyCache.cs
@@ -8,21 +8,26 @@ namespace WalletWasabi.Blockchain.Keys;
 
 public class HdPubKeyCache : IEnumerable<HdPubKeyInfo>
 {
-	private Dictionary<Script, HdPubKey> _hdPubKeyIndexedByScriptPubKey = new(1_000);
-	private HdPubKeyInfo[] _hdPubKeyInfos = new HdPubKeyInfo[1_000];
-	private int _hdKeyCount = 0;
-
-	private ArraySegment<HdPubKeyInfo> Snapshot =>
-		new (_hdPubKeyInfos, 0, _hdKeyCount);
+	private int _currentCapacity = 1_000;
+	private readonly Dictionary<Script, HdPubKeyInfo> _hdPubKeyIndexedByScriptPubKey = new(1_000);
 
 	public IEnumerable<HdPubKey> HdPubKeys =>
-		Snapshot.Select(x => x.HdPubKey);
+		_hdPubKeyIndexedByScriptPubKey.Values.Select(x => x.HdPubKey);
 
-	public bool TryGetPubKey(Script destination, [NotNullWhen(true)] out HdPubKey? hdPubKey) =>
-		_hdPubKeyIndexedByScriptPubKey.TryGetValue(destination, out hdPubKey);
+	public bool TryGetPubKey(Script destination, [NotNullWhen(true)] out HdPubKey? hdPubKey)
+	{
+		if (!_hdPubKeyIndexedByScriptPubKey.TryGetValue(destination, out var hdPubKeyInfo))
+		{
+			hdPubKey = null;
+			return false;
+		}
+
+		hdPubKey = hdPubKeyInfo.HdPubKey;
+		return true;
+	}
 
 	public HdPubKeyPathView GetView(KeyPath keyPath) =>
-		new(HdPubKeys.Where(x => x.FullKeyPath.Parent == keyPath));
+		new(_hdPubKeyIndexedByScriptPubKey.Values.Select(x => x.HdPubKey).Where(x => x.FullKeyPath.Parent == keyPath));
 
 	public IEnumerable<HdPubKey> AddRangeKeys(IEnumerable<HdPubKey> keys)
 	{
@@ -36,19 +41,18 @@ public class HdPubKeyCache : IEnumerable<HdPubKeyInfo>
 
 	public void AddKey(HdPubKey hdPubKey, ScriptPubKeyType scriptPubKeyType)
 	{
-		if (_hdPubKeyInfos.Length == _hdKeyCount)
+		if (_hdPubKeyIndexedByScriptPubKey.Count == _currentCapacity)
 		{
-			Array.Resize(ref _hdPubKeyInfos, 2 * _hdKeyCount);
+			_currentCapacity = _hdPubKeyIndexedByScriptPubKey.EnsureCapacity(2 * _currentCapacity);
 		}
 
 		var info = new HdPubKeyInfo(hdPubKey, scriptPubKeyType);
-		_hdPubKeyInfos[_hdKeyCount] = info;
-		_hdPubKeyIndexedByScriptPubKey[info.ScriptPubKey] = info.HdPubKey;
-		_hdKeyCount++;
+		_hdPubKeyIndexedByScriptPubKey[info.ScriptPubKey] = info;
 	}
 
 	public IEnumerator<HdPubKeyInfo> GetEnumerator() =>
-		Snapshot
+		_hdPubKeyIndexedByScriptPubKey
+			.Values
 			.OrderBy(x => x.HdPubKey.Index)
 			.GetEnumerator();
 
